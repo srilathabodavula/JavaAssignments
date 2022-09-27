0e84f46c74707ec5cdb0d2ba7abb2530cd0eb655
srilatha <srilu.it@gmail.com>
2022-09-27 15:32:09 -0500

    Day 7 Assignment

--- Day7Assignment.java
+++ Day7Assignment.java
@@ -1,21 +1,49 @@
-import java.util.ArrayList;
-import java.util.Comparator;
 
-import javax.swing.text.html.HTMLDocument.Iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Day7Assignment {
 
 	
 	
-	public Day7Assignment(String fname, String lname, float d) {
+	private String fname1;
+	private String lname1;
+	private float gpaf;
+
+
+	public Day7Assignment(String fname, String lname, float gpa) {
 		
 		// TODO Auto-generated constructor stub
-		String firstname = fname;
-		String lastname = lname;
-		float gpascore = d;
+		 this.fname1 = fname;
+		 this.lname1 = lname;
+		 this.gpaf = gpa;
+		
+		
 		
 		
 	}
+	
+	
+	public String getfname()
+	{
+		
+		return this.fname1;
+	}
+	public String getlname1()
+	{
+		
+				return this.lname1;
+	}
+	
+	public float getgpaf()
+	{
+		
+		return this.gpaf;
+	}
+	
+	
 	
 	
 		// TODO Auto-generated method stub
@@ -25,21 +53,65 @@ public class Day7Assignment {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Day7Assignment p1 = new Day7Assignment("Srilatha", "Bodavula", 7.5f);
-		Day7Assignment p2 = new Day7Assignment("Steve", "McGhee", 9.5f);
-		Day7Assignment p3 = new Day7Assignment("abcd", "xyz", 6.5f);
-		Day7Assignment p4 = new Day7Assignment("abcd", "xyz", 6.5f);
-		ArrayList<Object> student = new ArrayList<>();
-		student.add(p1);
-		student.add(p2);
-		student.add(p3);
-		student.add(p4);
-		System.out.println(student.get(3).equals(p4));
 		
-	//	System.out.println(student.get(0).;
-		System.out.println(p1.toString().indexOf(2));
 		
-		System.out.println(p1.hashCode());
+		LinkedList<Day7Assignment> studentlist = new LinkedList<>();
+		studentlist.add(new Day7Assignment(" "," ",0));
+		studentlist.add(new Day7Assignment("Srilatha", "Bodavula", 7.5f));
+		studentlist.add(new Day7Assignment("Steve", "McGhee", 9.5f));
+		studentlist.add(new Day7Assignment("abcd", "xyz", 6.5f));
+		
+		int l = studentlist.size();
+		System.out.println(l);
+		
+		for(int i=0;i<l;i++)
+		{
+			System.out.println(studentlist.get(i).fname1);
+		}
+		
+		int m=1,j;
+		
+			for (j=1;j<l;j++)
+			{
+				m=j+1;
+				while(m < l)
+				{
+			if(studentlist.get(m).gpaf < studentlist.get(j).gpaf)
+			{
+				
+				
+				//System.out.println(studentlist.get(m).gpaf);
+				//System.out.println(studentlist.get(j).gpaf);
+				
+				
+				 studentlist.set(0, studentlist.get(m));
+				 studentlist.set(m, studentlist.get(j));
+				 studentlist.set(j, studentlist.get(0));
+			//	 System.out.println(studentlist.get(0).gpaf);
+				 //studentlist.set(0, null);
+				 }
+		m++;
+			
+			}
+				
+			
+			
+		}
+		
+		studentlist.removeFirst();
+		
+		 	  		 for(m=0; m<studentlist.size();m++)
+		 	  		 System.out.println(studentlist.get(m).gpaf);
+		 
+			 
+			
+			  
+			  
+		  
+		  
+		  
+		 
+
 		
 		
 		
@@ -69,7 +141,7 @@ public class Day7Assignment {
 	
 	
 	
-	
+	}
 	
 	
 	
@@ -77,4 +149,6 @@ public class Day7Assignment {
 
 	}
 
-}
+		
+
+
